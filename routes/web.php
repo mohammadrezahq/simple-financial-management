1e6ee2c21d06705e5a5bc75100cf15318dd3ba92
mohammadrezahq <mohammadrezah.official@gmail.com>
2022-11-23 21:12:23 +0330

    feat: transaction filter route

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::post('/transactions/filter', [TransactionController::class, 'filter'])->name('transactions.filter');
     Route::resource('/transactions', TransactionController::class, [
         'names' => 'transactions'
     ]);
