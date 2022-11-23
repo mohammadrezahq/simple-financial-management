fd7e4b62e8b819f36aaea82edd31ec1b230fe5cf
mohammadrezahq <mohammadrezah.official@gmail.com>
2022-11-23 21:47:05 +0330

    feat: add filter method to transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\TransactionCreateRequest;
 use App\Models\Category;
 use App\Models\Transaction;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
@@ -16,9 +17,30 @@ class TransactionController extends Controller
      */
     public function index()
     {
-        $transactions = Transaction::get();
+        $transactions = Transaction::with('category')
+                            ->whereThisMonth()
+                            ->orderBy('id', 'DESC')
+                            ->get();
 
         return inertia('Transaction/Index', compact('transactions'));
+    }
+
+    public function filter(Request $request)
+    {
+        $request->validate([
+            'starts_at' => 'nullable|date',
+            'ends_at' => 'nullable|date'
+        ]);
+
+        $starts_at = $request->starts_at ? $request->starts_at : verta()->startMonth()->formatGregorian('Y-n-j H:i:s');
+        $ends_at = $request->ends_at ? $request->ends_at : Carbon::now();
+
+        $transactions = Transaction::with('category')->whereDate('created_at', '>=', $starts_at)
+                    ->whereDate('created_at', '<=', $ends_at)
+                    ->orderBy('id', 'DESC')
+                    ->get();
+
+        return response()->json($transactions);
     }
 
     /**
