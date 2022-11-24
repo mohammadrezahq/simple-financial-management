9426e83662a3c3196742186d481fc98279d95f0f
mohammadrezahq <mohammadrezah.official@gmail.com>
2022-11-25 00:18:15 +0330

    feat: add toasts, linked categories and header text to create category page

--- resources/js/Pages/Category/Create.vue
+++ resources/js/Pages/Category/Create.vue
@@ -1,5 +1,4 @@
 <script setup>
-import Checkbox from "@/Components/Checkbox.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
@@ -7,6 +6,9 @@ import PrimaryButton from "@/Components/PrimaryButton.vue";
 import TextInput from "@/Components/TextInput.vue";
 import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
 import { Inertia } from "@inertiajs/inertia";
+import { Icon } from "@iconify/vue";
+import { errorToast, successToast } from "@/toasts";
+import HeaderText from "@/Components/HeaderText.vue";
 
 defineProps({
     categories: Object,
@@ -18,26 +20,33 @@ const form = useForm({
 
 const submit = () => {
     form.post(route("categories.store"), {
-        onSuccess: () => form.reset(),
+        onSuccess: () => {
+            form.reset();
+            successToast.show("دسته با موفقیت اضافه شد.");
+        },
     });
 };
 
 const remove = (id) => {
-    Inertia.delete(route("categories.destroy", id));
+    if (confirm("آیا از حذف این دسته مطمین می باشید؟")) {
+        Inertia.delete(route("categories.destroy", id), {
+            onSuccess: () => {
+                errorToast.show("دسته با موفقیت حذف شد.");
+            },
+        });
+    }
 };
 </script>
 
 <template>
     <AuthenticatedLayout>
-        <Head title="اضافه کردن" />
+        <Head title="اضافه کردن دسته" />
 
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                اضافه کردن
-            </h2>
+            <HeaderText> اضافه کردن دسته </HeaderText>
         </template>
 
-        <div class="py-12">
+        <div class="py-8">
             <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
@@ -68,19 +77,26 @@ const remove = (id) => {
                             </div>
                         </form>
                         <div class="mt-4">
+                            <h3 class="text-xl font-semibold p-4">دسته ها</h3>
                             <div
                                 v-for="(c, i) in categories"
                                 :key="c"
                                 class="inline-flex bg-gray-100 rounded-lg p-4 m-2 gap-x-2 justify-center items-center"
                             >
-                                <div class="text-lg font-bold">
+                                <Link
+                                    :href="route('categories.show', c.id)"
+                                    class="text-lg font-bold"
+                                >
                                     {{ c.name }}
-                                </div>
+                                </Link>
                                 <button
-                                    class="hover:bg-gray-200 px-2 rounded-full"
+                                    class="hover:bg-gray-200 p-2 rounded-full flex items-center"
                                     @click="remove(c.id)"
                                 >
-                                    x
+                                    <Icon
+                                        class="inline-block"
+                                        icon="ic:round-close"
+                                    />
                                 </button>
                             </div>
                         </div>
