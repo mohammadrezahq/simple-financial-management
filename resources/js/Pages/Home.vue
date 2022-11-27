c96b99c0ed58190395f94c6158187f5ebb7df418
mohammadrezahq <mohammadrezah.official@gmail.com>
2022-11-27 04:10:32 +0330

    feat: complete home page with incomes and changing style

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -7,12 +7,12 @@ const props = defineProps({
     transactions: Object,
 });
 
-const sumOfTransactions = ref(0);
+const sumOfExpenses = ref(0);
+const sumOfIncomes = ref(0);
+
 const categoryTable = ref({});
 
 props.transactions.map((t) => {
-    sumOfTransactions.value += t.amount;
-
     let category = "بدون دسته";
     let id = 0;
 
@@ -23,13 +23,26 @@ props.transactions.map((t) => {
 
     if (!categoryTable.value[category]) {
         categoryTable.value[category] = {
-            item: 1,
-            amount: t.amount,
+            expenses_count: 0,
+            incomes_count: 0,
+            expenses_amount: 0,
+            incomes_amount: 0,
             id,
         };
-    } else {
-        categoryTable.value[category]["item"]++;
-        categoryTable.value[category]["amount"] += t.amount;
+    }
+
+    if (t.type == "expense") {
+        sumOfExpenses.value += t.amount;
+
+        categoryTable.value[category]["expenses_count"]++;
+        categoryTable.value[category]["expenses_amount"] += t.amount;
+    }
+
+    if (t.type == "income") {
+        sumOfIncomes.value += t.amount;
+
+        categoryTable.value[category]["incomes_count"]++;
+        categoryTable.value[category]["incomes_amount"] += t.amount;
     }
 });
 </script>
@@ -44,11 +57,11 @@ props.transactions.map((t) => {
             </h2>
         </template>
 
-        <div class="py-12">
+        <div class="py-8">
             <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-                        <div class="flex">
+                        <div class="flex gap-x-4">
                             <div
                                 class="bg-gray-100 rounded-lg p-4 flex flex-col justify-center items-center"
                             >
@@ -56,24 +69,83 @@ props.transactions.map((t) => {
                                     جمع خرج این ماه
                                 </div>
                                 <div>
-                                    {{ numberWithCommas(sumOfTransactions) }}
+                                    -
+                                    {{ numberWithCommas(sumOfExpenses) }}
+                                    تومان
+                                </div>
+                            </div>
+                            <div
+                                class="bg-gray-100 rounded-lg p-4 flex flex-col justify-center items-center"
+                            >
+                                <div class="text-lg font-bold">
+                                    جمع درآمد این ماه
+                                </div>
+                                <div>
+                                    +
+                                    {{ numberWithCommas(sumOfIncomes) }}
+                                    تومان
+                                </div>
+                            </div>
+                            <div
+                                class="bg-gray-100 rounded-lg p-4 flex flex-col justify-center items-center"
+                            >
+                                <div class="text-lg font-bold">تراز</div>
+                                <div>
+                                    {{
+                                        numberWithCommas(
+                                            sumOfIncomes - sumOfExpenses
+                                        )
+                                    }}
                                     تومان
                                 </div>
                             </div>
                         </div>
-                        <div class="mt-4">
+                        <div class="mt-8">
                             <h2 class="font-bold text-xl">
                                 هزینه ها به تفکیک دسته
                             </h2>
                             <Link
                                 v-for="(ct, key) in categoryTable"
                                 :key="ct"
-                                class="inline-flex bg-gray-100 rounded-lg p-4 m-2 flex-col justify-center items-center"
+                                class="inline-flex gap-y-2 bg-gray-100 rounded-lg p-4 m-2 flex-col justify-center items-center"
                                 :href="route('categories.show', ct.id)"
                             >
                                 <div class="text-lg font-bold">{{ key }}</div>
-                                <div>
-                                    {{ numberWithCommas(ct.amount) }} تومان
+                                <div
+                                    class="flex gap-x-2 w-full justify-between"
+                                >
+                                    <span
+                                        class="bg-gray-200 text-gray-400 px-4 py-2 rounded-lg"
+                                    >
+                                        {{ ct.expenses_count }}
+                                    </span>
+                                    <span
+                                        class="p-2 bg-red-100 rounded-lg text-red-400"
+                                    >
+                                        -
+                                        {{
+                                            numberWithCommas(ct.expenses_amount)
+                                        }}
+                                        تومان
+                                    </span>
+                                </div>
+                                <div
+                                    class="flex gap-x-2 w-full justify-between"
+                                >
+                                    <span
+                                        class="bg-gray-200 text-gray-400 px-4 py-2 rounded-lg"
+                                    >
+                                        {{ ct.incomes_count }}
+                                    </span>
+                                    <span
+                                        class="p-2 bg-green-200 rounded-lg text-green-500"
+                                    >
+                                        +
+                                        {{
+                                            numberWithCommas(ct.incomes_amount)
+                                        }}
+                                        تومان
+                                    </span>
                                 </div>
                             </Link>
                         </div>
