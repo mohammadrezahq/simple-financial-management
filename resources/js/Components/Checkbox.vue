634fced71dba8785feab8d3b7d0ccf8a5dd274b6
mohammadrezahq <mohammadrezah.official@gmail.com>
2022-11-27 05:00:10 +0330

    some small style changes

--- resources/js/Components/Checkbox.vue
+++ resources/js/Components/Checkbox.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { computed } from 'vue';
+import { computed } from "vue";
 
-const emit = defineEmits(['update:checked']);
+const emit = defineEmits(["update:checked"]);
 
 const props = defineProps({
     checked: {
@@ -19,12 +19,16 @@ const proxyChecked = computed({
     },
 
     set(val) {
-        emit('update:checked', val);
+        emit("update:checked", val);
     },
 });
 </script>
 
 <template>
-    <input type="checkbox" :value="value" v-model="proxyChecked"
-           class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500">
+    <input
+        type="checkbox"
+        :value="value"
+        v-model="proxyChecked"
+        class="rounded border-gray-300 text-blue-600 shadow-sm focus:ring-blue-500"
+    />
 </template>
